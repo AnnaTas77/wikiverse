c0b364bb8034a617fca17590214144ce92f4b43f
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-16 12:23:51 +0100

    Created a Page component

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,34 +1,57 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import apiURL from '../api';
+import React, { useCallback, useEffect, useState } from "react";
+import apiURL from "../api";
+import Page from "./Page";
 
 const App = () => {
-  const [pages, setPages] = useState([])
+  const [pages, setPages] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(null);
 
   // Wrapped function in useCallback instead of useEffect because we will need
   // to call it outside the Effect later after making POST requests.
   const fetchPages = useCallback(async () => {
-    const response = await fetch(`${apiURL}/wiki`)
-    const pages = await response.json()
-    setPages(pages)
-  }, [])
+    const response = await fetch(`${apiURL}/wiki`);
+    const pages = await response.json();
+    setPages(pages);
+  }, []);
+
+  const handleClick = async (event) => {
+    event.preventDefault(); // stop the browser from following the link
+
+    const currentSlug = event.target.pathname;
+
+    const response = await fetch(apiURL + currentSlug);
+
+    const page = await response.json();
+
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
-    fetchPages()
-  }, [fetchPages])
+    fetchPages();
+  }, [fetchPages]);
+
+  if (currentPage) {
+    return <Page currentPage={currentPage} setCurrentPage={setCurrentPage} />;
+  }
 
   return (
-		<main>
+    <main>
       <h1 className="title">Wikiverse</h1>
-			<p className="subtitle">
+      <p className="subtitle">
         An interesting <span aria-label="library">📚</span>
       </p>
-			<ul className="pageList">
+      <ul className="pageList">
         {pages.map((page) => (
-          <li className="pageList-item" key={page.id}>{page.title}</li>
+          <li className="pageList-item" key={page.id}>
+            <a href={`/wiki/${page.slug}`} onClick={handleClick}>
+              {page.title}
+            </a>
+          </li>
         ))}
       </ul>
-		</main>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ public/react/components/Page.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Page({ currentPage, setCurrentPage }) {
+  return (
+    <main>
+      <a
+        href="/"
+        handleClick={(event) => {
+          event.preventDefault();
+          setCurrentPage(null);
+        }}
+      >
+        Home
+      </a>
+      <h1>{currentPage.title}</h1>
+      <p>
+        <b>Author:</b> {currentPage.author.name}
+      </p>
+      <p>
+        <b>Published:</b>
+        {new Date(currentPage.createdAt).toLocaleDateString()}
+      </p>
+      <p>
+        <b>Tags</b>
+      </p>
+      <ul>
+        {currentPage.tags.map((tag) => (
+          <li key={tag.id}>{tag.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default Page;
